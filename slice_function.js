3478a1be08fb65a0f7d2cd1b248b8dd2e8b7fcba
Mahadi Hassan <35952201+mahadi-nsu@users.noreply.github.com>
2019-09-09 11:43:24 +0600

    Update slice_function.js

--- slice_function.js
+++ slice_function.js
@@ -1,23 +1,16 @@
 const list = [1,2,3,100,500,600,7,8,9,10];
- const index = 4;
- 
- console.log("before :" +   list)
- 
- const increment = (list,index) =>{
-   
+const index = 4;
+console.log("before :" +   list);
+
+const increment = (list,index) =>{   
    return [
       ...list.slice(0,index),
       list[index] + 1 , 
       ...list.slice(index+1)
-      
      ]
-   
  }
+console.log("After : " + increment(list,4))
  
- console.log("After : " + increment(list,4))
- 
-
 //  result
-
 //  "before :1,2,3,100,500,600,7,8,9,10"
 // "After : 1,2,3,100,501,600,7,8,9,10"
